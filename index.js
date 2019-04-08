d1253e14a2ece8efc57c4ae29a94df054ac63b75
hondamadeline <35236254+mhonda19@users.noreply.github.com>
2019-04-08 17:53:33 -0500

    Potential Final Push

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
 console.log('JQuery is READY!');
-console.log($);
 
-var x = 'a string variable';
+var x = 'Test String Variable to Count';
+console.count(x);
 console.log(x); 
 
-//console.log(document);
-//console.log($(document));
-//console.log($(document));
-console.log($('#myID p'));
-var $mydiv = $('#myID');
-var mydiv = document.getElementById('#myID');
-
-console.log(mydiv);
-
-/*$( document ).ready(function() {
-  // Handler for .ready() called. will only do function once everything on page is ready
-    console.log("I am ready");
-});*/
+console.count(x);
